0598a30e079679d0fe6b206c3c83e81e9a8c48f6
Muhamhamza123 <126794203+Muhamhamza123@users.noreply.github.com>
2024-01-04 23:50:16 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ app = Flask(__name__, static_folder='../w3data/build', static_url_path='/')
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-    @app.errorhandler(404)
+@app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
 CORS(app, supports_credentials=True)
